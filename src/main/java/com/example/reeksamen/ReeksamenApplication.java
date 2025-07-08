71dd300469ae1353cf73f1a43f4abc40fcf50fdb
nauar123 <nauarbahaa@gmail.com>
2025-07-08 15:15:39 +0200

    oprettet
    d

---
+++ src/main/java/com/example/reeksamen/ReeksamenApplication.java
@@ -0,0 +1,13 @@
+package com.example.reeksamen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReeksamenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReeksamenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/reeksamen/ReeksamenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reeksamen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReeksamenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
