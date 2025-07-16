b6035f1b0b20e91ef452fdff93ad26da0e4fc402
nauar123 <nauarbahaa@gmail.com>
2025-07-16 16:43:17 +0200

    model klasserne lavet og repo mangler dog medarbejder og login

--- src/main/java/com/example/reeksamen/repository/MedarbejderRepo.java
+++ src/main/java/com/example/reeksamen/repository/MedarbejderRepo.java
@@ -1,5 +1,11 @@
 package com.example.reeksamen.repository;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.stereotype.Repository;
+import java.util.List;
 public class MedarbejderRepo {
 
 
