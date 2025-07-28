bbbc6bb0db05812bfe584ed83767acb0ca876a11
Rache <Rachel_melese@hotmail.dk>
2025-07-28 17:59:15 +0200

    random

--- src/main/java/com/example/reeksamen/repository/MedarbejderRepo.java
+++ src/main/java/com/example/reeksamen/repository/MedarbejderRepo.java
@@ -1,12 +1,35 @@
 package com.example.reeksamen.repository;
 
+import com.example.reeksamen.model.Medarbejder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.stereotype.Repository;
 import java.util.List;
-public class MedarbejderRepo {
+@Repository
+public class MedarbejderRepo
+{
+@Autowired
+    JdbcTemplate template;
+
+
+// Henter en liste over alle medarbejdere
+public List <Medarbejder> fetchAll()
+{
+    String sql = "SELECT * FROM medarbejder";
+    RowMapper<Medarbejder> rowMapper = new BeanPropertyRowMapper<>(Medarbejder.class);
+    return template.query(sql,rowMapper);
+}
+
+// Finder en medarbejder via email
+public Medarbejder findByEmail (String email)
+{
+    String sql = "SELECT * FROM medarbejder WHERE email = ?";
+    RowMapper<Medarbejder> rowMapper = new BeanPropertyRowMapper<>(Medarbejder.class);
+    return template.queryForObject(sql,rowMapper,email);
+}
+
 
 
 
--- src/test/java/com/example/reeksamen/ReeksamenApplicationTests.java
+++ src/test/java/com/example/reeksamen/ReeksamenApplicationTests.java
@@ -1,13 +1,33 @@
 package com.example.reeksamen;
 
+import com.example.reeksamen.model.Bil;
+import com.example.reeksamen.service.bilService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class ReeksamenApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+    @Autowired
+    private bilService bilservice;
 
+    @Test
+
+    public void testAddBil() {
+        System.out.println("Testen starter");
+        Bil bil = new Bil(0, "VW", "Golf", "2020", "random", Bil.Status.LEDIG, 22.22, 22.22);
+        bilservice.addBil(bil);
+        System.out.println("Bilen er oprettet");
+
+        List<Bil> biler = bilservice.getAllBil();
+        System.out.println("bilen er tilføjet til listen");
+        assertTrue(biler.size() > 0, "Bilen blev ikke tilføjet til listen");
+    }
 }
+
+
