80ed50767ebc1096806a10befcdc7c2b633df3c9
Rache <Rachel_melese@hotmail.dk>
2025-08-01 14:07:58 +0200

    test slettet

--- src/test/java/com/example/reeksamen/ReeksamenApplicationTests.java
+++ src/test/java/com/example/reeksamen/ReeksamenApplicationTests.java
@@ -16,37 +16,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 @SpringBootTest
 class ReeksamenApplicationTests {
 
-    @Autowired
-    private bilService bilservice;
-
-    @Autowired
-    private lejekontraktService lejekontraktService;
-
-    @Test
-
-    public void testAddBil() {
-        System.out.println("Testen  testAddBil starter");
-        Bil bil = new Bil(0, "VW", "Golf", "2020", "random", Bil.Status.LEDIG, 22.22, 22.22);
-        bilservice.addBil(bil);
-        System.out.println("Bilen er oprettet");
-
-        List<Bil> biler = bilservice.fetchAll();
-        System.out.println("bilen er tilføjet til listen");
-        assertTrue(biler.size() > 0, "Bilen blev ikke tilføjet til listen");
-    }
-
-@Test
-    public void testAddLejekontrakt()
-    {
-        System.out.println("Testen addLejekontrakt starter");
-        Lejekontrakt lejekontrakt = new Lejekontrakt(0,2,1, 1, LocalDate.of(2025,7,28),LocalDate.of(2025,8,28), Lejekontrakt.AbonnementType.LIMITED, 2999.00);
-        lejekontraktService.addLejekontrakt(lejekontrakt);
-        System.out.println("Lejekontrakt er oprettet");
-
-        List<Lejekontrakt> lejekontrakter = lejekontraktService.fetchAll();
-        System.out.println("Lejekontrakten er tilføjet til listen");
-        assertTrue(lejekontrakter.size() > 0,"Lejekontrakt blev ikke tilføjet til listen");
-    }
+ //Slettet
 }
 
 
