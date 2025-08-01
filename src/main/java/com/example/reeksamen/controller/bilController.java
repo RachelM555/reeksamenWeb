bbd1e70c1e38b95530a2d08851648568c33e248b
nauar123 <nauarbahaa@gmail.com>
2025-08-01 13:23:17 +0200

    gået igng med bilController

--- src/main/java/com/example/reeksamen/controller/bilController.java
+++ src/main/java/com/example/reeksamen/controller/bilController.java
@@ -1,4 +1,27 @@
 package com.example.reeksamen.controller;
 
+import com.example.reeksamen.service.bilService;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class bilController {
+
+    @Autowired
+
+    bilService bilService;
+
+    // Metode til at tjekke om en bruger ikke er loggede ind, true hvis brugeren ikke er loggede ind.
+    private boolean ikkeLoggedInd(HttpSession session)
+    {
+        Boolean loggedeInd = (Boolean) session.getAttribute("loggedeInd");
+
+        return loggedeInd == null || !loggedeInd;
+    }
+
+
+
+
+
 }
