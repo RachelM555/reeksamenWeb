65e9d0188c8a884a5e355d38767880c22d44d320
Rache <Rachel_melese@hotmail.dk>
2025-08-06 16:30:38 +0200

    medarbejder service

--- src/main/java/com/example/reeksamen/service/medarbejderService.java
+++ src/main/java/com/example/reeksamen/service/medarbejderService.java
@@ -1,14 +1,30 @@
 package com.example.reeksamen.service;
+import com.example.reeksamen.model.Medarbejder;
+import com.example.reeksamen.repository.MedarbejderRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 
 @Service
-public class medarbejderService
-{
+public class medarbejderService {
 
+    @Autowired
+    private MedarbejderRepo medarbejderRepo;
 
+    public List<Medarbejder> fetchAll()
+    {
+        return medarbejderRepo.fetchAll();
+    }
 
+    public Medarbejder findByEmail(String email)
+    {
+        return medarbejderRepo.findByEmail(email);
+    }
 
-
+    public boolean validateLogin(String email, String adgangskode)
+    {
+        return medarbejderRepo.validateLogin(email, adgangskode);
+    }
 
 }
