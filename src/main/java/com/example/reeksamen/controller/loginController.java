e56525f7544046b88bd5fd2dcd11389fd71272ce
Rache <Rachel_melese@hotmail.dk>
2025-08-06 16:41:12 +0200

    login

--- src/main/java/com/example/reeksamen/controller/loginController.java
+++ src/main/java/com/example/reeksamen/controller/loginController.java
@@ -1,12 +1,14 @@
 package com.example.reeksamen.controller;
-
 import com.example.reeksamen.model.Login;
+import com.example.reeksamen.model.Medarbejder;
 import com.example.reeksamen.service.medarbejderService;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class loginController {
@@ -23,8 +25,22 @@ public class loginController {
 
 
     @PostMapping("/Login")
-    public String tjekLogin()
+    public String tjekLogin(@RequestParam String email, @RequestParam String adgangskode, HttpSession session, Model model)
     {
+        Medarbejder medarbejder = medarbejderService.findByEmail(email);
+
+        if (medarbejder != null && medarbejderService.validateLogin(email, adgangskode)) {
+            session.setAttribute("medarbejderId", medarbejder.getMedarbejderId());
+            session.setAttribute("medarbejderNavn", medarbejder.getNavn());
+            session.setAttribute("medarbejderRolle", medarbejder.getRolle());
+            session.setAttribute("loggedInd", true);
+            return "redirect:/dashboard";
+        }
+        else model.addAttribute("fejl","Forkert email eller adgangskode");
+        return "login";
+
+
+
 
     }
 
