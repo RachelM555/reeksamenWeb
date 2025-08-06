52bc843808675fc1ac8e271f395bd77dc7d58dda
Rache <Rachel_melese@hotmail.dk>
2025-08-06 15:18:32 +0200

    færdig med bil, html og controller

--- src/main/java/com/example/reeksamen/controller/bilController.java
+++ src/main/java/com/example/reeksamen/controller/bilController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.ArrayList;
@@ -72,6 +73,21 @@ public class bilController {
         return "bilOverblik";
     }
 
+    @PostMapping("/slet-bil/{id}")
+    public String sletBil(@PathVariable("id") int bilId, HttpSession session)
+    {
+        if(ikkeLoggedInd(session))
+        {
+            return "redirect:/login";
+        }
+
+        else bilService.deleteById(bilId);
+
+        return "redirect/bilOverblik";
 
 
+    }
 }
+
+
+
